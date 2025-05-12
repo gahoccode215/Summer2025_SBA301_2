147e1d8541fac3772d9b74ade3357ecff66de506
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-12 09:30:46 +0700

    init

---
+++ src/main/java/com/sba301/online_ticket_sales/OnlineTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package com.sba301.online_ticket_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sba301/online_ticket_sales/OnlineTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba301.online_ticket_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
