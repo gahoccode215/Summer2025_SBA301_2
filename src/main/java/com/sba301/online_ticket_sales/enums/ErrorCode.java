a7208e2b25109f15384410678f30fe3465e05798
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-14 03:19:50 +0700

    feat: config Security

---
+++ src/main/java/com/sba301/online_ticket_sales/enums/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.sba301.online_ticket_sales.enums;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    // AUTHENTICATION EXCEPTION (1000 - 1999)
+    UNAUTHENTICATED(1000, "Vui lòng đăng nhập để sử dụng chức năng này", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1001, "Không thể thực hiện chức năng này", HttpStatus.FORBIDDEN),
+    ;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    private final int code;
+    private final String message;
+    private final HttpStatusCode statusCode;
+}
