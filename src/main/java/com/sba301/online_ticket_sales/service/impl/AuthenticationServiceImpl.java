6cd9f1b536a2a3aba69c0ad9f5e73294514f28f5
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-14 12:26:44 +0700

    refactor register

--- src/main/java/com/sba301/online_ticket_sales/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/sba301/online_ticket_sales/service/impl/AuthenticationServiceImpl.java
@@ -6,6 +6,7 @@ import com.sba301.online_ticket_sales.dto.identity.UserCreationParam;
 import com.sba301.online_ticket_sales.dto.request.RegisterRequest;
 import com.sba301.online_ticket_sales.entity.User;
 //import com.sba301.online_ticket_sales.mapper.AuthenticationMapper;
+import com.sba301.online_ticket_sales.mapper.AuthenticationMapper;
 import com.sba301.online_ticket_sales.repository.IdentityClient;
 import com.sba301.online_ticket_sales.repository.UserRepository;
 import com.sba301.online_ticket_sales.service.AuthenticationService;
@@ -28,7 +29,7 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationServiceImpl implements AuthenticationService {
     IdentityClient identityClient;
-    //    AuthenticationMapper authenticationMapper;
+    AuthenticationMapper authenticationMapper;
     UserRepository userRepository;
 
     @Value("${idp.client-id}")
@@ -43,6 +44,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public void register(RegisterRequest request) {
         try {
+            log.info("CLIENT ID: {}", clientId);
+            // Create account in KeyCloak
+            // Exchange client Token
             var token = identityClient.exchangeToken(TokenExchangeParam.builder()
                     .grant_type("client_credentials")
                     .client_id(clientId)
@@ -51,7 +55,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                     .build());
             log.info("TokenInfo {}", token);
             // Create user with client Token and given info
-
             // Get userId of keyCloak account
             var creationResponse = identityClient.createUser(
                     "Bearer " + token.getAccessToken(),
@@ -69,17 +72,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                     .build()))
                             .build());
 
+            // Assign userId of keycloak to User
             String userId = extractUserId(creationResponse);
             log.info("UserId {}", userId);
 
-            User user = User.builder()
-                    .userId(userId)
-                    .fullName(request.getFullName())
-                    .password(request.getPassword())
-                    .gender(request.getGender())
-                    .birthDate(request.getBirthDate())
-                    .email(request.getEmail())
-                    .build();
+            User user = authenticationMapper.toUser(request);
+            user.setUserId(userId);
             userRepository.save(user);
         } catch (FeignException ex) {
             log.error(ex.getMessage());
