b1ff47195c98b5556a72dcc1893f5eecccb0de41
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-29 01:05:52 +0700

    genre mapper

---
+++ src/main/java/com/sba301/online_ticket_sales/mapper/GenreMapper.java
@@ -0,0 +1,10 @@
+package com.sba301.online_ticket_sales.mapper;
+
+import com.sba301.online_ticket_sales.dto.genre.request.GenreCreationRequest;
+import com.sba301.online_ticket_sales.dto.genre.response.GenreResponse;
+import com.sba301.online_ticket_sales.entity.Genre;
+
+public interface GenreMapper {
+    Genre toGenre(GenreCreationRequest request);
+    GenreResponse toGenreResponse(Genre genre);
+}
