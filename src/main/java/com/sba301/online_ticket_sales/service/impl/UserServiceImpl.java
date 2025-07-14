e81b0974d11000bc44c83c5fb25bbd9826821fdc
dominhit215@gmail.com <dominhit215@gmail.com>
2025-07-14 08:08:52 +0700

    add createCusOffline

--- src/main/java/com/sba301/online_ticket_sales/service/impl/UserServiceImpl.java
+++ src/main/java/com/sba301/online_ticket_sales/service/impl/UserServiceImpl.java
@@ -207,6 +207,7 @@ public class UserServiceImpl implements UserService {
   public UserResponse createQuickCustomer(QuickCustomerRequest request) {
     User currentUser = getUserAuthenticated();
     if (!currentUser.isStaff()) {
+
       throw new AppException(ErrorCode.ACCESS_DENIED);
     }
 
